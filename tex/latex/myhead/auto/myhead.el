cb064e5e2df5389668e05db1468f557a3de7a952
Rémi Vanicat <vanicat@debian.org>
2010-10-26 13:36:53 +0200

    Des améliorations

--- tex/latex/myhead/auto/myhead.el
+++ tex/latex/myhead/auto/myhead.el
@@ -15,11 +15,13 @@
      '("dr" 1)
      '("mseg" 1)
      '("seg" 1)
+     '("vcoord" 2)
      '("vectr" 1)
      '("dset" 2)
      '("set" 1)
      '("pa" 1)
      '("header" 1)
+     "nohead"
      "ExoName"
      "dsum"
      "doplus"
@@ -31,10 +33,6 @@
      "udl"
      "ovl"
      "vect"
-     "R"
-     "Q"
-     "N"
-     "Z"
      "id"
      "sh"
      "ch"
@@ -51,7 +49,11 @@
      "vk"
      "Oij"
      "Oijk"
-     "im")
+     "im"
+     "R"
+     "Q"
+     "N"
+     "Z")
     (TeX-run-style-hooks
      "comment"
      "sistyle"
@@ -62,17 +64,17 @@
      "amsmath"
      "amssymb"
      "amsthm"
-     "inputenc"
-     "utf8x"
      "amstext"
      "amsfonts"
      "fontenc"
      "T1"
+     "inputenc"
+     "utf8"
      "lastpage"
      "fancyhdr"
      "tikz"
      "variations"
      "pgf"
      "babel"
-     "frenchb")))
+     "francais")))
 
