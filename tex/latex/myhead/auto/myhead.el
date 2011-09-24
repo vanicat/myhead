f73dc6b56a8b57e7a31a176db7869941e847f61c
Rémi Vanicat <vanicat@debian.org>
2011-09-24 17:25:12 +0200

    Add algorithm to myhead

--- tex/latex/myhead/auto/myhead.el
+++ tex/latex/myhead/auto/myhead.el
@@ -24,6 +24,7 @@
     (TeX-add-symbols
      '("dx" ["argument"] 0)
      '("CM" ["argument"] 0)
+     '("Variables" ["argument"] 1)
      '("mr" 1)
      '("mdr" 1)
      '("dr" 1)
@@ -37,6 +38,8 @@
      '("header" 1)
      '("languagepath" 1)
      '("uselanguage" 1)
+     '("Resultat" 1)
+     '("Entrees" 1)
      "nomprenom"
      "nohead"
      "ExoName"
@@ -81,6 +84,7 @@
      "sistyle"
      "multicol"
      "enumerate"
+     "algorithmicx"
      "eurosym"
      "geometry"
      "amsmath"
