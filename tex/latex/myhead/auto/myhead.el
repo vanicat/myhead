9377f45aefe202ad4cb9c6883b483c7c8989ddcd
Rémi Vanicat <vanicat@debian.org>
2013-05-10 10:03:05 +0200

    Correcting inclusion bugs

--- tex/latex/myhead/auto/myhead.el
+++ tex/latex/myhead/auto/myhead.el
@@ -1,99 +1,15 @@
 (TeX-add-style-hook "myhead"
  (lambda ()
-    (LaTeX-add-environments
-     "Exo"
-     "Sol"
-     "Exem"
-     "Exems"
-     "Theo"
-     "Cor"
-     "Conq"
-     "Conqs"
-     "Def"
-     "Defs"
-     "Meth"
-     "Prop"
-     "Lem"
-     "Rap"
-     "Rem"
-     "Rems"
-     "Voc"
-     "Not"
-     "Appl"
-     "Appls"
-     "Preuve"
-     "myeqn")
     (TeX-add-symbols
-     '("dx" ["argument"] 0)
-     '("CM" ["argument"] 0)
-     '("header" ["argument"] 1)
-     '("NoVariables" ["argument"] 1)
-     '("Variables" ["argument"] 1)
-     '("mr" 1)
-     '("mdr" 1)
-     '("dr" 1)
-     '("mseg" 1)
-     '("interval" 2)
-     '("seg" 1)
-     '("vcoord" 2)
-     '("vectr" 1)
-     '("dset" 2)
-     '("set" 1)
-     '("pa" 1)
      '("languagepath" 1)
      '("uselanguage" 1)
-     '("Affect" 2)
-     '("Ecrire" 1)
-     '("Resultat" 1)
-     '("Entree" 1)
-     '("Entrees" 1)
-     "nomprenom"
-     "nohead"
-     "ExoName"
-     "dsum"
-     "doplus"
-     "dint"
-     "dlim"
-     "dprod"
-     "dsup"
-     "dinf"
-     "udl"
-     "ovl"
-     "vect"
-     "id"
-     "sh"
-     "ch"
-     "renum"
-     "enum"
-     "dotline"
-     "snum"
-     "slinez"
-     "sline"
-     "vu"
-     "vv"
-     "vw"
-     "vi"
-     "vj"
-     "vk"
-     "vO"
-     "Oij"
-     "OIJ"
-     "Oijk"
-     "framesecsub"
-     "framesec"
-     "framesub"
-     "EURbis"
-     "im"
-     "R"
-     "Q"
-     "N"
-     "Z")
+     "EURbis")
     (TeX-run-style-hooks
+     "myhead-org"
      "comment"
      "sistyle"
      "multicol"
      "enumerate"
-     "algorithmicx"
      "eurosym"
      "geometry"
      "amsmath"
@@ -111,6 +27,7 @@
      "tikz"
      "variations"
      "pgf"
+     ""
      "babel"
      "francais")))
 
